0fd7a2899056370cdd008f8b2c520e8144124455
Akshat Khosya <20106@iiitu.ac.in>
2021-09-27 16:36:10 +0530

    added singup schema and env file

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const PORT = 4000;
+const admissionRoutes = express.Router();
+const dotenv = require('dotenv')
+dotenv.config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(cors());
+app.use(bodyParser.json());
+console.log(process.env['MONGODBCRED']);
+mongoose.connect("mongodb+srv://"+process.env['MONGODBCRED']+"@cluster0.phmm3.mongodb.net/admissionPortal", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
 
-var app = express();
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const Schema = mongoose.Schema;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+let regis = new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use('/',admissionRoutes );
+admissionRoutes.route('/')
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+module.exports = mongoose.model('regis', regis);
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
 });
-
-module.exports = app;
-//ok
